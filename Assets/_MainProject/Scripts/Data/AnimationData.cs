db12e753f87a3ae002325d4ba8595e414296f20f
Erickson Cortez <cortezericksonb@gmail.com>
2024-05-08 16:24:39 +0800

    added animation for idle, walking, and running for each hair, outfit, and hat

---
+++ Assets/_MainProject/Scripts/Data/AnimationData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InterviewTask {
+	public class AnimationData {
+		public const string RUN_ANIMATION_NAME = "IsRunning";
+		public const string WALK_ANIMATION_NAME = "IsWalking";
+		public const string X_DIRECTION_ANIMATION_NAME = "XDirection";
+		public const string Y_DIRECTION_ANIMATION_NAME = "YDirection";
+	}
+}
--- Assets/_MainProject/Scripts/PlayerController.cs
+++ Assets/_MainProject/Scripts/PlayerController.cs
@@ -6,6 +6,9 @@ using UnityEngine.InputSystem;
 namespace InterviewTask {
 	public class PlayerController : MonoBehaviour {
 		[SerializeField] private float speed;
+		[SerializeField] private float runSpeed;
+
+		[SerializeField] private Animator animator;
 		private Transform playerTransform;
 		private Vector2 inputMovement;
 		private bool canInteract = false;
@@ -15,13 +18,26 @@ namespace InterviewTask {
 		}
 
 		private void Update() {
-			Vector2 inputVector = inputMovement;
-			playerTransform.position += (Vector3)inputVector * speed * Time.deltaTime;
-
+			float speedModifier = animator.GetBool(AnimationData.RUN_ANIMATION_NAME) ? runSpeed : speed;
+			Vector2 movement = inputMovement.normalized * Time.deltaTime * speedModifier;
+			playerTransform.position += (Vector3)movement;
 		}
 
 		public void Move(InputAction.CallbackContext context) {
 			inputMovement = context.ReadValue<Vector2>();
+			if(Mathf.Abs(inputMovement.normalized.magnitude) > 0) {
+				animator.SetFloat(AnimationData.X_DIRECTION_ANIMATION_NAME, Mathf.RoundToInt(inputMovement.x));
+				animator.SetFloat(AnimationData.Y_DIRECTION_ANIMATION_NAME, Mathf.RoundToInt(inputMovement.y));
+			}
+			animator.SetBool(AnimationData.WALK_ANIMATION_NAME, Mathf.Abs(inputMovement.normalized.magnitude) > 0);
+		}
+
+		public void Run(InputAction.CallbackContext context) {
+			if(context.started) {
+				animator.SetBool(AnimationData.RUN_ANIMATION_NAME, true);
+			} else if(context.canceled) {
+				animator.SetBool(AnimationData.RUN_ANIMATION_NAME, false);
+			}
 		}
 
 		public void Interact(InputAction.CallbackContext context) {
